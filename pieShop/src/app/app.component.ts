0b93fef281ddc9efc39c90171676cf6eb26f5579
Doug-26 <cjdelacruz267@gmail.com>
2025-05-08 14:26:00 +0800

    Add TopBar component with template and styles

--- pieShop/src/app/app.component.ts
+++ pieShop/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TopBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pieShop';
 }
